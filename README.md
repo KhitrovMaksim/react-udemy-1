69598b1c7f3b35bfcd44f95266e5e366482f7d79
Maksim <Khitrov.Maks@gmail.com>
2023-07-01 17:54:45 +0400

    Added app

---
+++ README.md
@@ -0,0 +1,7 @@
+# About
+Section 11: Advanced: Handling Side Effects, Using Reducers & Using the Context API
+# Features
+- useState
+- useEffect
+- useReducer
+- local storage
--- src/App.js
+++ src/App.js
@@ -1,31 +1,43 @@
-import React, { useState } from 'react';
+import React, {useState, useEffect} from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
 import MainHeader from './components/MainHeader/MainHeader';
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  const loginHandler = (email, password) => {
-    // We should of course check email and password
-    // But it's just a dummy/ demo anyways
-    setIsLoggedIn(true);
-  };
 
-  const logoutHandler = () => {
-    setIsLoggedIn(false);
-  };
+    useEffect(() => {
+        const isUserLoggedIn = localStorage.getItem('isLoggedIn')
 
-  return (
-    <React.Fragment>
-      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
-      <main>
-        {!isLoggedIn && <Login onLogin={loginHandler} />}
-        {isLoggedIn && <Home onLogout={logoutHandler} />}
-      </main>
-    </React.Fragment>
-  );
+        if (isUserLoggedIn === '1') {
+            setIsLoggedIn(true);
+        }
+    }, []);
+
+    const loginHandler = (email, password) => {
+        localStorage.setItem('isLoggedIn', '1');
+        setIsLoggedIn(true);
+    };
+
+    const logoutHandler = () => {
+        localStorage.setItem('isLoggedIn', '0');
+        setIsLoggedIn(false);
+    };
+
+    return (
+        <React.Fragment>
+            <MainHeader
+                isAuthenticated={isLoggedIn}
+                onLogout={logoutHandler}
+            />
+            <main>
+                {!isLoggedIn && <Login onLogin={loginHandler}/>}
+                {isLoggedIn && <Home onLogout={logoutHandler}/>}
+            </main>
+        </React.Fragment>
+    );
 }
 
 export default App;
